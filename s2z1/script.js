0e7bb820882262943ca8ea29ad30f04ec2625e0d
kz-labrat <danbarbarosh@gmail.com>
2025-05-12 12:16:24 +0300

    s2z1

---
+++ s2z1/script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll('.fade-in').forEach(el => {
+      el.classList.add('visible');
+    });
+  });
+
+  document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll('.fade-in').forEach(el => {
+      el.classList.add('visible');
+    });
+  });
+
+  
