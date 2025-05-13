a5f440db2da5c0790a9faef337768d1665e1c46f
kz-labrat <danbarbarosh@gmail.com>
2025-05-13 12:49:34 +0300

    FOLDERE

--- s2z1/script.js
+++ s2z1/script.js
@@ -1,3 +1,22 @@
+function loadHTML(selector, file) {
+  fetch(file)
+    .then(response => {
+      if (!response.ok) throw new Error(`Failed to load ${file}`);
+      return response.text();
+    })
+    .then(data => {
+      document.querySelector(selector).innerHTML = data;
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  loadHTML("#header-container", "header.html");
+  loadHTML("#footer-container", "footer.html");
+});
+
 document.addEventListener("DOMContentLoaded", () => {
     document.querySelectorAll('.fade-in').forEach(el => {
       el.classList.add('visible');
